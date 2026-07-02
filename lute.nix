3da39f4304a20f732890365bfb11b90b2880ccba
LukaDev <hi@lukadev.me>
2026-07-03 00:18:19 +0200

    fix: make it work again

--- lute.nix
+++ lute.nix
@@ -11,113 +11,119 @@
   perl,
   git,
   pkg-config,
-  tuneHash ? "sha256-4EzQmf0cAIUKVLQF7tjm2lXa1uLpE/XUMJ5QtJNwWuc=",
+  tuneHash ? "sha256-cGx4c02iOs0ccAmtl1spbkp57HDmVAP/PYC8gHVdasU=",
 }:
+let
+  baseAttrs = finalAttrs: {
+    version = "1.0.1-nightly.20260701";
 
-stdenv.mkDerivation (finalAttrs: {
-  pname = "lute";
-  version = "v1.0.1-nightly.20260519";
+    src = fetchFromGitHub {
+      owner = "luau-lang";
+      repo = "lute";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-X7NJtzOdPWQazqirL5zD5Ua0wnuADySHRFsAxGSTWxk=";
+    };
 
-  nativeBuildInputs = [
-    cmake
-    ninja
-    perl
-    pkg-config
-    git
-  ];
+    tune =
+      runCommand "lute-${finalAttrs.version}-tune"
+        {
+          src = "${finalAttrs.src}/extern";
+          nativeBuildInputs = [
+            nix-prefetch-git
+            cacert
+          ];
+          outputHash = tuneHash;
+          outputHashMode = "recursive";
+        }
+        ''
+          mkdir -p "$out"
 
-  src = fetchFromGitHub {
-    owner = "luau-lang";
-    repo = "lute";
-    tag = finalAttrs.version;
-    hash = "sha256-5Z6JTa/sEVk1WvlT1tAQso0Z/i6uukejSKLJ3YYMHmo=";
+          for file in "$src"/*.tune; do
+            if [[ -f "$file" ]]; then
+              echo "fetching $(basename "$file")"
+
+              name=$(grep '^name' "$file" | sed -E 's/^name *= *"(.*)"/\1/')
+              remote=$(grep '^remote' "$file" | sed -E 's/^remote *= *"(.*)"/\1/')
+              revision=$(grep '^revision' "$file" | sed -E 's/^revision *= *"(.*)"/\1/')
+
+              nix-prefetch-git --builder --url "$remote" --rev "$revision" --out "$out/$name"
+            fi
+          done
+        '';
+
+    preConfigure = ''
+      cp -a "$tune"/* extern
+      chmod -R +w extern
+    '';
+
+    nativeBuildInputs = [
+      cmake
+      ninja
+      perl
+      pkg-config
+      git
+    ];
+
+    cmakeFlags = [
+      "-GNinja"
+    ]
+    # this makes HTTPS work
+    ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [
+      "-DCURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt"
+    ];
+    ninjaFlags = [ "lute/cli/lute" ];
+
+    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/bo/boringssl/package.nix#L33
+    NIX_CFLAGS_COMPILE = toString (
+      lib.optionals stdenv.cc.isGNU [
+        # Needed with GCC 12 but breaks on darwin (with clang)
+        "-Wno-error=stringop-overflow"
+      ]
+      ++ lib.optionals stdenv.cc.isClang [
+        "-Wno-error=character-conversion"
+      ]
+    );
+
+    LUTE_VERSION_SUFFIX =
+      let
+        match = builtins.match ".+-(.+)" finalAttrs.version;
+      in
+      if match == null then null else builtins.head match;
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp lute/cli/lute $out/bin
+    '';
+
+    meta = {
+      mainProgram = "lute";
+    };
   };
 
-  tune =
-    runCommand "${finalAttrs.finalPackage.name}-tune"
-      {
-        src = "${finalAttrs.src}/extern";
-        nativeBuildInputs = [
-          nix-prefetch-git
-          cacert
-        ];
-        outputHash = tuneHash;
-        outputHashMode = "recursive";
-      }
-      ''
-        mkdir -p "$out"
-
-        for file in "$src"/*.tune; do
-          if [[ -f "$file" ]]; then
-            echo "fetching $(basename "$file")"
-
-            name=$(grep '^name' "$file" | sed -E 's/^name *= *"(.*)"/\1/')
-            remote=$(grep '^remote' "$file" | sed -E 's/^remote *= *"(.*)"/\1/')
-            revision=$(grep '^revision' "$file" | sed -E 's/^revision *= *"(.*)"/\1/')
-
-            nix-prefetch-git --builder --url "$remote" --rev "$revision" --out "$out/$name"
-          fi
-        done
-      '';
-
-  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/bo/boringssl/package.nix#L33
-  env.NIX_CFLAGS_COMPILE = toString (
-    lib.optionals stdenv.cc.isGNU [
-      # Needed with GCC 12 but breaks on darwin (with clang)
-      "-Wno-error=stringop-overflow"
-    ]
-    ++ lib.optionals stdenv.cc.isClang [
-      "-Wno-error=character-conversion"
-    ]
+  lute0 = stdenv.mkDerivation (
+    finalAttrs:
+    let
+      base = baseAttrs finalAttrs;
+    in
+    base
+    // {
+      pname = "lute0";
+      cmakeFlags = base.cmakeFlags ++ [ "-DLUTE_STDLESS=ON" ];
+    }
   );
+in
+stdenv.mkDerivation (
+  finalAttrs:
+  let
+    base = baseAttrs finalAttrs;
+  in
+  base
+  // {
+    pname = "lute";
+    nativeBuildInputs = base.nativeBuildInputs ++ [ lute0 ];
 
-  cmakeFlags = [
-    "-GNinja"
-  ]
-  # this makes HTTPS work
-  ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [
-    "-DCURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt"
-  ];
-
-  preConfigure = ''
-    cp -a "$tune"/* extern
-    chmod -R +w extern
-
-    mkdir lute/std/src/generated
-    cp tools/templates/std_impl.cpp lute/std/src/generated/modules.cpp
-    cp tools/templates/std_header.h lute/std/src/generated/modules.h
-
-    mkdir lute/cli/generated
-    cp tools/templates/cli_impl.cpp lute/cli/generated/commands.cpp
-    cp tools/templates/cli_header.h lute/cli/generated/commands.h
-
-    mkdir lute/batteries/generated
-    cp tools/templates/batteries_impl.cpp lute/batteries/generated/batteries.cpp
-    cp tools/templates/batteries_header.h lute/batteries/generated/batteries.h
-
-    mkdir lute/definitions/src/generated
-    cp tools/templates/definitions_impl.cpp lute/definitions/src/generated/modules.cpp
-    cp tools/templates/definitions_header.h lute/definitions/src/generated/modules.h
-  '';
-
-  buildPhase = ''
-    ninja lute/cli/lute -v \
-      && ./lute/cli/lute run "$OLDPWD"/tools/luthier.luau generate lute \
-      && ninja lute/cli/lute -v
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp lute/cli/lute $out/bin
-  '';
-
-  doCheck = true;
-  nativeCheckInputs = [ writableTmpDirAsHomeHook ];
-  checkPhase = ''
-    ninja tests/lute-tests -v && ( cd "$OLDPWD" && "$OLDPWD"/tests/lute-tests )
-  '';
-
-  meta = {
-    mainProgram = "lute";
-  };
-})
+    preConfigure = base.preConfigure + ''
+      lute tools/luthier.luau generate lute
+    '';
+  }
+)
